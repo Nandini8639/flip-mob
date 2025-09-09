3635228ddc678b8d02574a6d92662767ead9a818
Nandini8639 <mathakunandini@gmail.com>
2025-09-09 18:14:20 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,46 @@
+import Mobile from './components/Mobile'
+function App(){
+  return(
+    <>
+    <Mobile
+    image="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/k/8/y/-original-imahexhjqbbh4zn2.jpeg?q=70"
+    name="REDMI Note 14 SE 5G (Mystique White, 128 GB)  (6 GB RAM)"
+    f1="6 GB RAM | 128 GB ROM"
+    f2="16.94 cm (6.67 inch) Full HD+ Display"
+    f3="50MP + 8MP + 2MP | 20MP Front Camera"
+    f4="5110 mAh Battery"
+    f5="Dimensity 7025 Ultra Processor"
+    old="₹14,999"
+    new="19,999"
+    ></Mobile>
+
+    <br></br>
+        <Mobile
+    image="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/c/1/g/-original-imahexhjnh9weebd.jpeg?q=70"
+    name="REDMI Note 14 SE 5G (Mystique White, 128 GB)  (6 GB RAM)"
+    f1="6 GB RAM | 128 GB ROM"
+    f2="16.94 cm (6.67 inch) Full HD+ Display"
+    f3="50MP + 8MP + 2MP | 20MP Front Camera"
+    f4="5110 mAh Battery"
+    f5="Dimensity 7025 Ultra Processor"
+    old="₹14,999"
+    new="19,999"
+    ></Mobile>
+    <br></br>
+    
+    <Mobile
+    image="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/r/5/z/-original-imahawgarmg5f8ff.jpeg?q=70"
+    name="REDMI Note 14 SE 5G (Mystique White, 128 GB)  (6 GB RAM)"
+    f1="6 GB RAM | 128 GB ROM"
+    f2="16.94 cm (6.67 inch) Full HD+ Display"
+    f3="50MP + 8MP + 2MP | 20MP Front Camera"
+    f4="5110 mAh Battery"
+    f5="Dimensity 7025 Ultra Processor"
+    old="₹14,999"
+    new="19,999"
+    ></Mobile>
+    
+    </>
+  )
+}
+export default App
